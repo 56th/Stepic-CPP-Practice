0d6654fa82c2997581a9fc359d43784981aa8e6b
Alexander Zhiliakov <alex@math.uh.edu>
2024-05-27 19:21:07 -0500

    Implement IntLength compile-time meta function

--- part-2/5.3.7/IntList.hpp
+++ part-2/5.3.7/IntList.hpp
@@ -7,3 +7,13 @@ struct IntList<N, Vals...> { // specialization for the length >= 1
     static int const Head = N;
     using Tail = IntList<Vals...>;
 };
+
+template<typename IL>
+struct Length {
+    static int const value = 1 + Length<typename IL::Tail>::value;
+};
+
+template<>
+struct Length<IntList<>> {
+    static int const value = 0;
+};
--- part-2/5.3.7/test.cpp
+++ part-2/5.3.7/test.cpp
@@ -1,9 +1,18 @@
 #include "IntList.hpp"
 
 using primes = IntList<2, 3, 5, 7, 11, 13>;
+
 static_assert(primes::Head == 2);
 static_assert(primes::Tail::Head == 3);
 static_assert(primes::Tail::Tail::Head == 5);
 static_assert(primes::Tail::Tail::Tail::Head == 7);
 static_assert(primes::Tail::Tail::Tail::Tail::Head == 11);
 static_assert(primes::Tail::Tail::Tail::Tail::Tail::Head == 13);
+
+static_assert(Length<primes>::value == 6);
+static_assert(Length<primes::Tail>::value == 5);
+static_assert(Length<primes::Tail::Tail>::value == 4);
+static_assert(Length<primes::Tail::Tail::Tail>::value == 3);
+static_assert(Length<primes::Tail::Tail::Tail::Tail>::value == 2);
+static_assert(Length<primes::Tail::Tail::Tail::Tail::Tail>::value == 1);
+static_assert(Length<primes::Tail::Tail::Tail::Tail::Tail::Tail>::value == 0);
