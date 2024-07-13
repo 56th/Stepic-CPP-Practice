20ec295051a774fd318758eafff1313ca2a1562f
Alexander Zhiliakov <alex.zhiliakov@protonmail.com>
2024-07-13 16:45:06 +0300

    Add IntCons + Generate meta funcs

--- part-2/5.3.7/IntList.hpp
+++ part-2/5.3.7/IntList.hpp
@@ -10,6 +10,8 @@ struct IntList<N, Vals...> { // specialization for the length >= 1
     static int const Length = 1 + sizeof...(Vals);
 };
 
+// Length meta function
+
 template<typename IL> // alternative to IntList<...>::Length
 struct Length {
     static int const value = 1 + Length<typename IL::Tail>::value;
@@ -18,4 +20,26 @@ struct Length {
 template<>
 struct Length<IntList<>> {
     static int const value = 0;
+};
+
+// IntCons meta function
+
+template<int N, typename IL>
+struct IntCons;
+
+template<int N, int... Vals>
+struct IntCons<N, IntList<Vals...>> {
+    using type = IntList<N, Vals...>;
+};
+
+// Generate meta function
+
+template<int N, int N0 = 0>
+struct Generate {
+    using type = typename IntCons<N0, typename Generate<N - 1, N0 + 1>::type>::type;
+};
+
+template<int N0>
+struct Generate<0, N0> {
+    using type = IntList<>;
 };
--- part-2/5.3.7/test.cpp
+++ part-2/5.3.7/test.cpp
@@ -1,3 +1,4 @@
+#include <type_traits>
 #include "IntList.hpp"
 
 using primes = IntList<2, 3, 5, 7, 11, 13>;
@@ -24,3 +25,28 @@ static_assert(Length<primes::Tail::Tail::Tail>::value == 3);
 static_assert(Length<primes::Tail::Tail::Tail::Tail>::value == 2);
 static_assert(Length<primes::Tail::Tail::Tail::Tail::Tail>::value == 1);
 static_assert(Length<primes::Tail::Tail::Tail::Tail::Tail::Tail>::value == 0);
+
+static_assert(std::is_same<
+    IntCons<1, IntList<>>::type, 
+    IntList<1>
+>::value);
+
+static_assert(std::is_same<
+    IntCons<1, IntList<2, 3, 4>>::type, 
+    IntList<1, 2, 3, 4>
+>::value);
+
+static_assert(std::is_same<
+    Generate<0>::type, 
+    IntList<>
+>::value);
+
+static_assert(std::is_same<
+    Generate<1>::type, 
+    IntList<0>
+>::value);
+
+static_assert(std::is_same<
+    Generate<3>::type, 
+    IntList<0, 1, 2>
+>::value);
